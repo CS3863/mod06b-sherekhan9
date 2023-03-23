5d06e8bdeb157dff46d77b8709e81fcbb37af604
sherekhan9 <Sherekhan9@gmail.com>
2023-03-23 13:41:51 -0500

    Mod06B-Kise

--- Mod06B-Kise/Assets/Scripts/MoveForward.cs
+++ Mod06B-Kise/Assets/Scripts/MoveForward.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed;
+    public float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 20.0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
--- Mod06B-Kise/Assets/Scripts/PlayerControler.cs
+++ Mod06B-Kise/Assets/Scripts/PlayerControler.cs
@@ -11,19 +11,25 @@ public class PlayerControler : MonoBehaviour
     }
 
     // Update is called once per frame
-    public GameObject projectileprefab; // NOT DONE
+    public GameObject projectilePrefab; // NOT DONE
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange;
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            //Launch a projectile from the player
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
        if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange)
+       if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
