656bb858d876ee9f5ae5450adb4dd080c7779f3b
sherekhan9 <Sherekhan9@gmail.com>
2023-03-28 12:54:44 -0500

    Mod06B

--- Mod06B-Kise/Assets/Scripts/SpawnManager.cs
+++ Mod06B-Kise/Assets/Scripts/SpawnManager.cs
@@ -19,10 +19,18 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S)) {
+
+            SpawnRandomAnimal();
+
+           
+        }
+        void SpawnRandomAnimal()
+        {
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), 
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
                 animalPrefabs[animalIndex].transform.rotation);
+
         }
     }
 }
