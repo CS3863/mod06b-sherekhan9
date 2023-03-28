3345ca5d2b838214d0f9a82334d0d02d5f2a7cdf
sherekhan9 <Sherekhan9@gmail.com>
2023-03-28 13:43:37 -0500

    Mod06b-Kise

--- Mod06B-Kise/Assets/Scripts/DetectCollsions.cs
+++ Mod06B-Kise/Assets/Scripts/DetectCollsions.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class DetectCollsions : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class DetectCollsions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+      //  transform.localScale *= 2;
+
+     
+     Destroy(gameObject);
+       Destroy(other.gameObject);
     }
 }
